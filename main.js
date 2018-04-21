36bcccd516dda229d880ce2e617e8db99b5840dd
Toshiyuki Masui <masui@pitecan.com>
2018-04-21 19:35:22 +0900

    ...

--- main.js
+++ main.js
@@ -1,41 +1,65 @@
 /*
-Copyright 2014 Google Inc. All rights reserved.
+ * 以下から借用
+ * https://github.com/google/extra-keyboards-for-chrome-os/blob/master/capslockremap/background.js
+ * (IME機能でCapsLockをCtrlにするもの)
+ */
 
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-
-    http://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-*/
 var contextID = -1;
+
 var lastRemappedKeyEvent = undefined;
 var ctrlKey = false;
 
 var japaneseMode = false;
-var pat = ""; // 日本語入力パタン e.g. "masui"
+var pat = "";              // 日本語入力パタン e.g. "masui"
 var candidates = [];
 
 chrome.input.ime.onFocus.addListener(function(context) {
-  contextID = context.contextID;
+    contextID = context.contextID;
 });
 
 chrome.input.ime.onBlur.addListener(function(context) {
-  contextID = -1;
+    contextID = -1;
 });
 
 function isRemappedEvent(keyData) {  
- // hack, should check for a sender ID (to be added to KeyData)
- return lastRemappedKeyEvent != undefined &&
+    // hack, should check for a sender ID (to be added to KeyData)
+    return lastRemappedKeyEvent != undefined &&
         (lastRemappedKeyEvent.key == keyData.key &&
          lastRemappedKeyEvent.code == keyData.code &&
          lastRemappedKeyEvent.type == keyData.type
         ); // requestID would be different so we are not checking for it  
+}
+
+function searchAndShowCands(){
+    candidates = [];
+    search(pat,0,function(word,pat,connection){
+	var newword = word.replace(/\*/g,'');
+	if(candidates.indexOf(newword) < 0){
+	    candidates.push(newword);
+	}
+    });
+    var candmenus = [];
+    for(var i=0;i<3 && i<candidates.length;i++){
+	candmenus.push({
+	    candidate:candidates[i],
+	    id:i
+	});
+    }
+    chrome.input.ime.setCandidates({
+	contextID:contextID,
+	candidates:candmenus
+    });
+}
+
+function showComposition(text){
+    var obj = {
+	contextID:contextID,
+	text:text,
+	cursor:text.length,
+	selectionStart: 0,
+	selectionEnd: text.length
+    };
+    chrome.input.ime.setComposition(obj); // カーソル位置に未変換文字列をアンダーライン表示
 }
 
 chrome.input.ime.onKeyEvent.addListener(
@@ -47,32 +71,6 @@ chrome.input.ime.onKeyEvent.addListener(
             return false;
 	}
 
-	if (keyData.type == "keydown" && keyData.key == "x"){
-	    chrome.input.ime.setCandidateWindowProperties({
-		engineID:engineID,
-		properties:{
-		    visible:true,
-		    cursorVisible:false,
-		    vertical:true,
-		    pageSize:3,
-		    auxiliaryText: "Lexierra",
-		    auxiliaryTextVisible: false
-		}
-	    });
-	    var obj = {
-		contextID:contextID,
-		text:"かんじ",
-		cursor:0,
-		selectionStart: 0,
-		selectionEnd: 2
-	    };
-	    chrome.input.ime.setComposition(obj); // 未変換文字列を表示
-
-	    return true;
-
-	    //chrome.input.ime.commitText({"contextID": contextID, "text": "xxxxxx"});
-	}
-
 	if (keyData.key == ";" && keyData.code){
 	    keyData.key = "Enter";
 	    keyData.code = "Enter";
@@ -81,33 +79,6 @@ chrome.input.ime.onKeyEvent.addListener(
 	    handled = true;
 	}
 	
-	if (keyData.type == "keydown" && keyData.code == "AltRight" && !japaneseMode){
-	    japaneseMode = true;
-	    chrome.input.ime.setCandidateWindowProperties({
-		engineID:engineID,
-		properties:{
-		    visible:true,
-		    cursorVisible:false,
-		    vertical:true,
-		    pageSize:3,
-		    auxiliaryText: "Lexierra",
-		    auxiliaryTextVisible: false
-		}
-	    });
-	    pat = "";
-	    handled = false;
-	}
-	if (keyData.type == "keydown" && keyData.code == "AltLeft" && japaneseMode){
-	    japaneseMode = false;
-	    chrome.input.ime.setCandidateWindowProperties({
-		engineID:engineID,
-		properties:{
-		    visible:false
-		}
-	    });
-	    handled = false;
-	}
-
 	if (keyData.code == "ControlRight"){
 	    keyData.ctrlKey = false;
 	    if(keyData.shiftKey){
@@ -181,9 +152,40 @@ chrome.input.ime.onKeyEvent.addListener(
             handled = true;
 	}
 
+	if (keyData.type == "keydown" && keyData.code == "AltRight" && !japaneseMode){
+	    japaneseMode = true;
+	    chrome.input.ime.setCandidateWindowProperties({
+		engineID:engineID,
+		properties:{
+		    visible:true,
+		    cursorVisible:false,
+		    vertical:true,
+		    pageSize:3,
+		    auxiliaryText: "Lexierra",
+		    auxiliaryTextVisible: false
+		}
+	    });
+	    pat = "";
+	    handled = false;
+	}
+	if (keyData.type == "keydown" && keyData.code == "AltLeft" && japaneseMode){
+	    japaneseMode = false;
+	    chrome.input.ime.setCandidateWindowProperties({
+		engineID:engineID,
+		properties:{
+		    visible:false
+		}
+	    });
+	    handled = false;
+	}
+
+	// 日本語入力処理
+
 	if(japaneseMode){
 	    if(keyData.type == "keydown" && keyData.key.match(/^[a-z]$/)){
 		pat += keyData.key;
+		showComposition(pat)
+		/*
 		var obj = {
 		    contextID:contextID,
 		    text:pat,
@@ -192,29 +194,10 @@ chrome.input.ime.onKeyEvent.addListener(
 		    selectionEnd: pat.length
 		};
 		chrome.input.ime.setComposition(obj); // 未変換文字列を表示
+		 */
 	    }
 
-	    // searchAndShowCands();
-
-	    candidates = [];
-	    search(pat,0,function(word,pat,connection){
-		var newword = word.replace(/\*/g,'');
-		if(candidates.indexOf(newword) < 0){
-		    candidates.push(newword);
-		}
-	    });
-	    var candlines = [];
-	    for(var i=0;i<3 && i<candidates.length;i++){
-		candlines.push({
-		    candidate:candidates[i],
-		    id:i
-		});
-	    }
-	    chrome.input.ime.setCandidates({
-		contextID:contextID,
-		candidates:candlines
-	    });
-
+	    searchAndShowCands();
 
 	    return true;
 	}
@@ -223,3 +206,4 @@ chrome.input.ime.onKeyEvent.addListener(
 	return handled;
     }
 );
+
